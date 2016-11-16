3d5996512a00c8795567cc9b7e10f100ecdd1d5b
Michael Clifford <cliffom@gmail.com>
2016-11-16 07:22:18 -0800

    DRYing things up

--- lib/tokenex.ex
+++ lib/tokenex.ex
@@ -11,18 +11,21 @@ defmodule TokenEx do
       "APIKey" => api_key}
   end
 
+  def make_request(action, request) do
+    response = post(action, request)
+    {:ok, response}
+  end
+
   def tokenize(data, token_scheme, config) do
     request = %{"Data" => data,
       "TokenScheme" => token_scheme} |> Map.merge(config)
 
-    response = post("/Tokenize", request)
-    {:ok, response}
+    make_request("/Tokenize", request)
   end
 
   def detokenize(token, config) do
     request = %{"Token" => token} |> Map.merge(config)
 
-    response = post("/Detokenize", request)
-    {:ok, response}
+    make_request("/Detokenize", request)
   end
 end
